d3c00d1236aeca2fa288ad9287af8c0d3e5a0d5a
Dhiraj Upadhyaya <dup1966@gmail.com>
2018-10-27 16:43:57 +0530

    a

--- 3d1-DT-CHAID-usvote.R
+++ 3d1-DT-CHAID-usvote.R
@@ -2,7 +2,7 @@
 # require(rsample) # for dataset and splitting also loads broom and tidyr
 #install.packages("CHAID", repos="http://R-Forge.R-project.org")
 library(CHAID)  #library for performing CHAID decision tree
-
+#used when all variables are categories
 #Dataset
 data(USvote)  #from lib CHAID
 ?USvote
@@ -11,7 +11,10 @@ str(USvote)
 #Quick CHAID analysis
 set.seed(101)
 sample1 = USvote[sample(1:nrow(USvote), 1000),]
-chaidModel <- chaid(vote3 ~ ., data = sample1, control=chaid_control(minbucket = 20, minsplit=7, minprob=0))
+chaidModel = chaid(vote3 ~ ., data = sample1, control=chaid_control(minbucket = 20, minsplit=7, minprob=0))
+#minsplit	Number of observations in splitted response at which no further split is desired.
+#minbucket Minimum number of observations in terminal nodes.
+#minprob	Mininimum frequency of observations in terminal nodes.
 print(chaidModel)
 plot(chaidModel)
-
+?chaid
